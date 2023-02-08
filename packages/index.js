044f5dc264aba13e5d0b46714597b70f2166ca2a
Leonardo-Ferreira-costa <62809356+Leonardo-Ferreira-costa@users.noreply.github.com>
2023-02-08 19:20:54 -0300

    Ajuste monorepo Vercel

---
+++ packages/index.js
@@ -0,0 +1,8 @@
+const PROJECT = {
+  title: "Crud Reactjs + Mysql ",
+  subtitle: "Monorepo",
+};
+
+export function getSiteInfo() {
+  return { title: PROJECT.title, subtitle: PROJECT.subtitle };
+}
