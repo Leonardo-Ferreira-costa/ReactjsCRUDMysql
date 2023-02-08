95f89ca245c08c1b9cf2867c8ec98b3a051dc89c
Leonardo-Ferreira-costa <62809356+Leonardo-Ferreira-costa@users.noreply.github.com>
2023-02-08 19:20:23 -0300

    Delete package directory

--- package/index.js
+++
@@ -1,8 +0,0 @@
-const PROJECT = {
-  title: "Crud Reactjs + Mysql ",
-  subtitle: "Monorepo",
-};
-
-export function getSiteInfo() {
-  return { title: PROJECT.title, subtitle: PROJECT.subtitle };
-}
